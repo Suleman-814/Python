c728561ebe1efaf49018e74c250356e3771c832f
suleman-814 <suleman0814@gmail.com>
2025-10-01 21:34:31 +0530

    Add basic python programs and intro text

---
+++ Basic Python Programs.py
@@ -0,0 +1,30 @@
+#Hello World Program
+
+print("Hello, World!")
+
+####################################################################################################################################
+
+#Addition of Two Numbers Program
+
+a = 10
+b = 5
+sum = a + b
+print("The sum is:", sum)
+
+####################################################################################################################################
+
+#Print User's Name Program
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+####################################################################################################################################
+
+#Area of a Rectangle Program
+
+length = 6
+width = 4
+area = length * width
+print("Area of rectangle is:", area)
+
+
